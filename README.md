e1490f20b61716a986042a529fed82bc04c69981
Huan Nguyen <famish99@gmail.com>
2022-04-14 17:06:02 -0500

    refactor(build): updated to Docker workflow

--- README.md
+++ README.md
@@ -4,26 +4,96 @@ Automated resume generation tool that converts a YAML-defined resume into HTML v
 
 ## Setup
 
-Expects `template/` directory with Jinja formatted HTML file and any associated files needed for rendering.
+### Prerequisites
 
-## Build instructions
+Requires that you have Docker Desktop setup.
 
-Jinsei can run single shot or autobuild mode.
+### Configure Run Parameters
 
-### Build once:
+Create an `.env` file with the following parameters:
 
-```bash
-./jinsei.py <input yaml> <output pdf>
+```env
+INPUT_FILE=<input resume YAML path>
+METHOD=(once|auto)
+OUTPUT_FILE=<output PDF path>
+OVERRIDES=<optional: override resume YAML paths>
+TEMPLATE_DIR=<template directory>
 ```
 
-### Autobuild on change:
+### Directory Structure
+
+Expects directory with Jinja formatted HTML file and any associated files needed for rendering such as style sheets under your `TEMPLATE_DIR`.
+
+### Running the Application
+
+Jinsei can run single shot or autobuild mode which can be configured using the `METHOD` variable.
+
+To run the application:
 
 ```bash
-./jinsei.py --method auto <input yaml> <output pdf>
+docker-compose up
 ```
 
-## Requires:
-* Python 3 (targeting 3.5)
-  * ruamel.yaml
-  * Jinja2
-* wkhtmltopdf - Needed for HTML to PDF conversion stage
+## Resume Template Format
+
+Top level keys for the resume YAML are the following:
+
+ * `template`: Path of the HTML template file
+ * `name`: Full name
+ * `email`
+ * `phone`
+ * `address`: Primary street address line
+ * `address_2`: (Optional) Second street address line
+ * `city`
+ * `state`
+ * `zipcode`
+ * `title`: Current Job title
+ * `profile`: A summary line
+ * `skills`: A list of technical skills
+ * `experience`: Outlined in [Experience Fields](#experience-fields)
+ * `education`: Outlined in [Education Fields](#experience-fields)
+ * `projects`: Outlined in [Projects Fields](#experience-fields)
+
+### Experience Fields
+
+Each `experience` item expects the following fields:
+
+ * `employer`
+ * `title`
+ * `location`
+ * `start_date`
+ * `end_date`
+ * `tasks`: A list of projects and responsibilites held in the position
+
+### Education Fields
+
+Each `education` item expects the following fields:
+
+ * `institution`
+ * `location`
+ * `start_date`
+ * `end_date`
+ * `degree`
+
+### Projects Fields
+
+Each `projects` item expects the following fields:
+
+ * `title`
+ * `description`
+ * `link`: `https://` will automatically be prepended to this value, normalizing the printed vs pdf version.
+
+### Overrides
+
+`OVERRIDES` will allow you to specify multiple files that you may not want to track in the repo but hold data relevant to your resume for example:
+
+```yaml
+email: thats@secret.com
+phone: 555-555-5555
+address: 221B Baker Street
+city: Westminster
+state: London
+zipcode: NW1
+```
+
+Entries under `skills`, `experience`, `education`, `project` will be appended to the base resume file, the rest will be replaced with the last specified file taking precedence.
--- jinsei.py
+++ jinsei.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
+import argparse
 from argparse import ArgumentParser
 from jinja2 import Environment, FileSystemLoader
 from ruamel import yaml
 from shutil import copyfile
 from time import sleep, strftime
+from typing import Any
 import subprocess
 import sys
 import traceback
@@ -14,67 +15,89 @@ import os
 NUM_COLS = 3
 
 
-def build_resume(args):
-    os.makedirs('build', exist_ok=True)
-    for f_name in os.listdir(args.template_dir):
-        if '.css' in f_name:
-            copyfile(
-                os.path.join(args.template_dir, f_name),
-                os.path.join('build', f_name)
-                )
-    env = Environment(loader=FileSystemLoader(args.template_dir))
-    with open(args.input, 'r') as ifp:
+def parse_context(input_file: str, overrides: list[str]) -> dict[str, Any]:
+    context: dict[str, Any] = {}
+    with open(input_file, 'r') as ifp:
         # Needed for cases file reports a modified time but changes are cached
         ifp.flush()
-        context = yaml.load(ifp, Loader=yaml.Loader)
-        template = env.get_template(context['template'])
-        ir_path = os.path.join('build', 'out_%s' % context['template'])
-        with open(ir_path, 'w') as ofp:
-            skill_len = len(context['skills'])
-            skills = (
-                context['skills'] + [
-                    ' '
-                    for _ in range(
-                        (skill_len % NUM_COLS) and
-                        NUM_COLS - (skill_len % NUM_COLS)
-                        )
-                    ]
+        context.update(yaml.load(ifp, Loader=yaml.Loader))
+    for override in overrides:
+        with open(override, 'r') as ifp:
+            # Needed for cases file reports a modified time but changes are cached
+            ifp.flush()
+            over_dict: dict[str, Any] = yaml.load(ifp, Loader=yaml.Loader)
+            extensions: list[Any]
+            if extensions := over_dict.pop('skills', None):
+                context['skills'].extend(extensions)
+            if extensions := over_dict.pop('experience', None):
+                context['experience'].extend(extensions)
+            if extensions := over_dict.pop('education', None):
+                context['education'].extend(extensions)
+            if extensions := over_dict.pop('projects', None):
+                context['projects'].extend(extensions)
+            context.update(over_dict)
+    return context
+
+
+def build_resume(build_args: argparse.Namespace) -> None:
+    os.makedirs('build', exist_ok=True)
+    for f_name in os.listdir(build_args.template_dir):
+        if '.css' in f_name:
+            copyfile(
+                os.path.join(build_args.template_dir, f_name),
+                os.path.join('build', f_name)
+            )
+    context = parse_context(build_args.input, build_args.overrides)
+    env = Environment(loader=FileSystemLoader(build_args.template_dir))
+    template = env.get_template(context['template'])
+    skill_len = len(context['skills'])
+    skills = (
+        context['skills'] + [
+            ' '
+            for _ in range(
+                (skill_len % NUM_COLS) and
+                NUM_COLS - (skill_len % NUM_COLS)
                 )
-            context['skills'] = [skills[i::NUM_COLS] for i in range(NUM_COLS)]
-            ofp.write(template.render(context))
-            ofp.flush()
-        subprocess.run([
-            'wkhtmltopdf',
-            '-s', 'Letter',
-            '--print-media-type',
-            '--enable-local-file-access',
-            ir_path,
-            args.output
-            ])
+            ]
+        )
+    context['skills'] = [skills[i::NUM_COLS] for i in range(NUM_COLS)]
+    ir_path = os.path.join('build', 'out_%s' % context['template'])
+    with open(ir_path, 'w') as ofp:
+        ofp.write(template.render(context))
+        ofp.flush()
+    subprocess.run([
+        'wkhtmltopdf',
+        '-s', 'Letter',
+        '--print-media-type',
+        '--enable-local-file-access',
+        ir_path,
+        build_args.output
+        ])
 
 
-def auto_build_resume(args):
-    print('Watching for changes - %s' % args.input)
+def auto_build_resume(build_args: argparse.Namespace) -> None:
+    print('Watching for changes - %s' % build_args.input, file=sys.stderr)
 
     def last_updated_time():
         return max(
-            os.stat(args.input).st_mtime,
+            os.stat(build_args.input).st_mtime,
+            *(os.stat(override).st_mtime for override in build_args.overrides),
             *(
-                os.stat(os.path.join(args.template_dir, filename)).st_mtime
-                for filename in os.listdir(args.template_dir)
+                os.stat(os.path.join(build_args.template_dir, filename)).st_mtime
+                for filename in os.listdir(build_args.template_dir)
                 if '.sw' not in filename
                 )
             )
 
     last_updated = last_updated_time()
     try:
-        build_resume(args)
+        build_resume(build_args)
         while True:
             if last_updated_time() != last_updated:
                 last_updated = last_updated_time()
-                print('rebuilding - %s' % strftime('%I:%M:%S'))
+                print('rebuilding - %s' % strftime('%I:%M:%S'), file=sys.stderr)
                 try:
-                    build_resume(args)
+                    build_resume(build_args)
                 except Exception:
                     traceback.print_exc(file=sys.stdout)
             sleep(5)
@@ -86,6 +109,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('input')
     parser.add_argument('output')
+    parser.add_argument('--overrides', dest='overrides', nargs='*', default=[])
     parser.add_argument('--template-dir', dest='template_dir', default='templates')
     parser.add_argument('--method', dest='method', choices=['once', 'auto'], default='once')
     args = parser.parse_args()
